4b07e1b12f56d0ab1bc90b71ba22a35b793ccc9e
Grzegorz Kodrzycki <g.kodrzycki20@gmail.com>
2023-10-21 11:53:11 +0200

    Creating model

--- src/database/utils.py
+++ src/database/utils.py
@@ -1,6 +1,7 @@
 from connector import connect_with_connector_auto_iam_authn
-from sqlalchemy import insert, select
-from tables import User
+from sqlalchemy import insert, select, join
+from tables import User, Inventory
+# from model import get_model
 
 
 def get_user_by_username(username: str):
@@ -31,8 +32,33 @@ def add_user(username, email, password):
         conn.execute(query)
         conn.commit()
 
-# def for model
 
-#
+def add_plant(user_id, plant_id, photo_id, weight=0, age=0, height=0):
+    pool = connect_with_connector_auto_iam_authn()
 
+    with pool.connect() as conn:
+        query = insert(Inventory).values(
+            user_id=user_id,
+            plant_id=plant_id,
+            photo_id=photo_id,
+            weight=weight,
+            age=age,
+            height=height,
+        )
+        conn.execute(query)
+        conn.commit()
+
+
+def creating_model():
+    pool = connect_with_connector_auto_iam_authn()
+
+    with pool.connect() as conn:
+        query = select(Inventory.id, Inventory.plant_id).select_from(User).join(Inventory, User.id == Inventory.user_id)
+        conn.execute(query)
+        conn.commit()
+
+    # model = get_model()
+
+
+creating_model()
 
