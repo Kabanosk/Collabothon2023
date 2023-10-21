c4fb448e66ee0d9dcb5fc3905661cd882a805cb7
pawel-kojma <pawel.kojma@hotmail.com>
2023-10-21 17:30:53 +0200

    define user routes

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,17 +1,36 @@
-from dataclasses import dataclass
+from fastapi import APIRouter, Request, Form, status
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from functools import wraps
+from fastapi.responses import HTMLResponse
 
-import sqlalchemy
+router = APIRouter()
+templates = Jinja2Templates(directory="src/templates")
 
 
-@dataclass
-class User:
-    id: int
-    login: str
-    password: str
-    email: str
+# Every function using this decorator
+# must accept fastapi.Request as 1st argument
+def auth_required(f):
+    @wraps(f)
+    def check_session(*args,**kwargs):
+        request = args[0]
+        if not request.session.get('user')
+            return templates.TemplateResponse("login.html",{"error": "Not Authenticated"})
+        f(*argc,**kwargs)
+    return check_session
 
-    def insert_to_db(self):
-        pass
 
-    def add_image(self):
-        pass
+@router.get('/profile')
+def profile(request: Reqeust):
+    return templates.TemplateResponses("profile.html",{"request":request})
+
+@router.get('/profile/stats')
+def profile_stats(request: Request):
+    return templates.TemplateResponses("profile_stats.html",{"request",request})
+
+@router.get('/profile/badges')
+def profile_badges(request: Request):
+    return templates.TemplateResponses("profile_badges.html",{"request",request})
+
+
