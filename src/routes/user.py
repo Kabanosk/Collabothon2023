cbe44e7483833617e661da3d2f3b5125a73243a6
kabanosk <56364007+Kabanosk@users.noreply.github.com>
2023-10-21 17:43:51 +0100

    Changes after conde review

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,36 +1,35 @@
-from fastapi import APIRouter, Request, Form, status
-from fastapi.responses import RedirectResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 from functools import wraps
-from fastapi.responses import HTMLResponse
+
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
-templates = Jinja2Templates(directory="src/templates")
+templates = Jinja2Templates(directory="templates")
 
 
 # Every function using this decorator
 # must accept fastapi.Request as 1st argument
 def auth_required(f):
     @wraps(f)
-    def check_session(*args,**kwargs):
+    def check_session(*args, **kwargs):
         request = args[0]
-        if not request.session.get('user')
-            return templates.TemplateResponse("login.html",{"error": "Not Authenticated"})
-        f(*argc,**kwargs)
+        if not request.session.get('user'):
+            return templates.TemplateResponse("login.html", {"error": "Not Authenticated"})
+        f(*args, **kwargs)
+
     return check_session
 
 
 @router.get('/')
-def profile(request: Reqeust):
-    return templates.TemplateResponses("profile.html",{"request":request})
+def profile(request: Request):
+    return templates.TemplateResponse("profile.html", {"request": request})
+
 
 @router.get('/stats')
 def profile_stats(request: Request):
-    return templates.TemplateResponses("profile_stats.html",{"request",request})
+    return templates.TemplateResponse("profile_stats.html", {"request": request})
+
 
 @router.get('/badges')
 def profile_badges(request: Request):
-    return templates.TemplateResponses("profile_badges.html",{"request",request})
-
-
+    return templates.TemplateResponse("profile_badges.html", {"request": request})
--- src/routes/view.py
+++ src/routes/view.py
@@ -1,15 +1,10 @@
-from fastapi import APIRouter, Request, Form, status
-from fastapi.responses import RedirectResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-from functools import wraps
-from fastapi.responses import HTMLResponse
 
 router = APIRouter()
 templates = Jinja2Templates(directory="src/templates")
 
+
 @router.get('/')
-def profile(request: Reqeust):
-    return templates.TemplateResponses("home.html",{"request":request})
-
-
+def profile(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
