42e9d82a384730eca87110279775532b97a3e39e
pawel-kojma <pawel.kojma@hotmail.com>
2023-10-21 21:44:58 +0200

    add badges script

--- scripts/add_badges_db.py
+++ scripts/add_badges_db.py
@@ -12,8 +12,8 @@ with pool.connect() as conn:
     print('connection success')
     for file in [x for x in p.iterdir() if not x.is_dir()]:
         description = file.stem
-        #print(file,data,description)
+        path = '/' + str(short(file.resolve(),4))
         print(f'Adding {description}')
-        print(f'resolved {short(file.resolve(),4)}')
-        #add_badge_to_db(data,description)
+        print(f'resolved {path}')
+        add_badge_to_db(path,description)
 
