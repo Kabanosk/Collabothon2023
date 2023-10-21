6a77d57a734158c68ce04c7ff241910063bb8691
Wojciech Fiołka <56364007+Kabanosk@users.noreply.github.com>
2023-10-21 11:53:55 +0100

    Get sql data proper extraction (#6)

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,8 +1,10 @@
-import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 
 from .recommender_model import Model
 
 
 def get_model(sql_data: list) -> Model:
-    data = np.array(sql_data)
-    return Model(data)
+    x_text = [list(map(str, sublist)) for sublist in sql_data]
+    count_vectorizer = CountVectorizer()
+    data_matrix = count_vectorizer.fit_transform(x_text).toarray()
+    return Model(data_matrix)
