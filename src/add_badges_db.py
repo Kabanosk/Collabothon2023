916281592c69a462412b1ed4f905c23ea312835e
pawel-kojma <pawel.kojma@hotmail.com>
2023-10-21 20:59:41 +0200

    type

--- src/add_badges_db.py
+++ src/add_badges_db.py
@@ -7,7 +7,7 @@ p = Path('../static/img/badges')
 
 with pool.connect() as conn:
     print('connection success')
-    for file in [x for x in p.iterdir() is not p.is_dir()]:
+    for file in [x for x in p.iterdir() if not p.is_dir()]:
         data = file.read_bytes()
         description = file.stem
         add_badge_to_db(data,description)
