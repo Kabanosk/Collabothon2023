0828c018004e501141e74c8b2370487b0cd14d04
JarekKadecki <95314594+JarekKadecki@users.noreply.github.com>
2023-10-22 01:17:26 +0200

    Plant input form improvements

--- static/js/add_plant.js
+++ static/js/add_plant.js
@@ -3,36 +3,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const file_input = document.querySelector("#img_input")
     const name_input = document.querySelector("#name_input")
-    const output = document.querySelector("#img_output")
     let imagesArray = []
 
-    file_input.addEventListener("change", () => {
-        const file = file_input.files
-        imagesArray.push(file[0])
-        document.getElementById('img_output').innerHTML = imagesArray[0].name
-    })
+    // Get all elements with the class "plant_input"
+    const plant_inputs = document.querySelectorAll(".plant_input");
 
-    name_input.addEventListener("change", () => {
-        console.log("chuj");
-        if(imagesArray[0].name != "" && name_input.value != "")
-        {
-            document.getElementById("submit_button").style.display = "block";
-        }
-        else
-        {
-            document.getElementById("submit_button").style.display = "none ";
-        }
-    })
+    // Add a change event listener to each input element
+    plant_inputs.forEach((element) => {
+        element.addEventListener("change", () => {
+            const all_inputs_set = [...plant_inputs].every((input) => input.value.trim() !== "");
+
+            if (all_inputs_set) {
+                document.querySelector("#submit_button").style.display = "block";
+                // document.querySelector("#submit_button").style.visibility = "visible";
+                // document.querySelector("#submit_button").style.opacity = "1";
+                
+            } else {
+                document.querySelector("#submit_button").style.display = "none";
+                // document.querySelector("#submit_button").style.visibility = "hidden";
+                // document.querySelector("#submit_button").style.opacity = "0";
+            }
+        });
+    });
 
     const displayFormButton = document.querySelector("#display_form_button");
     const addPlantInputWrapper = document.querySelector("#add_plant_input_wrapper");
     
     displayFormButton.addEventListener("click", () => {
         event.preventDefault();
-        if (addPlantInputWrapper.style.display !== "none") {
-            addPlantInputWrapper.style.display = "none";
+        if (addPlantInputWrapper.style.visibility !== "visible") {
+            //addPlantInputWrapper.style.display = "none";
+            addPlantInputWrapper.style.visibility = "visible";
+            addPlantInputWrapper.style.opacity = "1";
         } else {
-            addPlantInputWrapper.style.display = "block";
+            //addPlantInputWrapper.style.display = "block";
+            addPlantInputWrapper.style.visibility = "hidden";
+            addPlantInputWrapper.style.opacity = "0";
         }
     });
 
