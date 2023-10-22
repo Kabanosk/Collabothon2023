60af6a9a67a5127e63befdadb33056e36e1d22eb
pawel-kojma <pawel.kojma@hotmail.com>
2023-10-22 03:23:03 +0200

    index page

--- src/main.py
+++ src/main.py
@@ -10,16 +10,16 @@ from model import get_model
 from routes.auth import router as auth_router
 from routes.inventory import router as plant_router
 from routes.user import router as profile_router
+from routes.view import router as view_router
 
 load_dotenv()
 
 
 def create_app() -> FastAPI:
     _app = FastAPI()
-
     _model = get_model(data_for_model())
     _model.save()
-
+    _app.include_router(view_router)
     _app.include_router(auth_router)
     _app.include_router(plant_router, prefix="/inventory")
     _app.include_router(profile_router, prefix="/profile")
