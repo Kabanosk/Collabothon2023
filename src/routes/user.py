74e97d2b1e13e98de08c2b899b25771e747deedf
pawel-kojma <pawel.kojma@hotmail.com>
2023-10-22 05:19:46 +0200

    add check for quantity of plants of user for model

--- src/routes/user.py
+++ src/routes/user.py
@@ -18,24 +18,27 @@ def profile(request: Request):
     user = request.session.get("user")
     if not user:
         return RedirectResponse("/login", status_code=status.HTTP_303_SEE_OTHER)
-    user_vec = model.data[user['id']]
-    recommended_users = model.get_recommended_users(user_vec.reshape((1, -1)))
-    keys = [list(model.data.keys())[i] for i in sorted(recommended_users[0])]
-    ids = [np.nonzero(model.data[u])[0] for u in keys]
 
-    recommended_plants_id = set(np.concatenate(ids)).difference(
-        set(list(np.nonzero(user_vec)[0])))
-    plants = np.array(get_all_plants_from_db())
-    plants_d = {}
-    for arr in plants:
-        plants_d[arr[0]] = arr
+    recommended_plants = None
+    if user['id'] in model.data:
+        user_vec = model.data[user['id']]
+        recommended_users = model.get_recommended_users(user_vec.reshape((1, -1)))
+        keys = [list(model.data.keys())[i] for i in sorted(recommended_users[0])]
+        ids = [np.nonzero(model.data[u])[0] for u in keys]
 
-    recommended_plants = [plants_d[x] for x in recommended_plants_id]
+        recommended_plants_id = set(np.concatenate(ids)).difference(
+            set(list(np.nonzero(user_vec)[0])))
+        plants = np.array(get_all_plants_from_db())
+        plants_d = {}
+        for arr in plants:
+            plants_d[arr[0]] = arr
+
+        recommended_plants = [plants_d[x] for x in recommended_plants_id]
 
     user_badges = get_user_badge(user['id'])
     return templates.TemplateResponse(
         "profile.html",
-        {"request": request, "badges": user_badges},
+        {"request": request,"recommended_plants":recommended_plants, "badges": user_badges},
     )
 
 
@@ -48,6 +51,6 @@ def profile_stats(request: Request):
 def profile_badges(request: Request):
     user = request.session.get('user')
     if not user:
-        return RedirectResponse('/login')
+        return RedirectResponse('/login',status_code=status.HTTP_303_SEE_OTHER)
     user_badges = get_user_badge(user['id'])
     return templates.TemplateResponse("badges.html", {"request": request, "badges": user_badges})
